ccf04d620ffb3da9c7cbe43998a276998784b985
Ray Araujo <rayssa.araujo@taglivros.com.br>
2022-08-02 11:59:52 -0300

    create text interface for metadata

---
+++ src/Interfaces/RestaurantTexts.ts
@@ -0,0 +1,17 @@
+interface Texts {
+  contact: string
+  description: string
+  hours: string
+  payment: string
+  price: string
+  site: string
+}
+
+export const RestaurantTexts: Texts = {
+  'contact': 'Contato',
+  'description': 'Descrição',
+  'hours': 'Horário de funcionamento',
+  'payment': 'Forma de pagamento',
+  'price': 'Faixa de preço',
+  'site': 'Site',
+}
